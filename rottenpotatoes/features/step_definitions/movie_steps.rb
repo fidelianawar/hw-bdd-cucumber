01b52f6b480e8b467e3b373d534fdb1eb395f9fa
Fidelia Nawar <f_nawar@coloradocollege.edu>
2018-11-05 15:04:12 -0700

    changing movie_steps.rb

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -2,10 +2,10 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
+    Movie.create!(movie)
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
-  fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -17,8 +17,11 @@ end
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
+  title = 1
+  body = page.body.match('<td.+<\/td>')
+  movies = body.find_all { |i| body.find_index(i) % 4 == 0 }
+  	expect(movies.index(e1)).to be < movies.index(e2)
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -29,10 +32,13 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
 end
 
 Then /I should see all the movies/ do
+  movies = Movie.all
+  movies.each do |movie|
+    assert page.body =~ /#{movie.title}/, "Missing #{movie.title} "
+  end
+
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
 end
